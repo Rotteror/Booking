ac76000ee06646a6daab5fdfb661093004a344bf
Rotteror <xeziod@gmail.com>
2021-06-19 17:54:04 +0300

    Modify CSS file , implement sorting func for Hotels

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 
 router.get('/', async (req, res) => {
-    const hotels = await req.storage.getAllHotels();
- 
+    let hotels = await req.storage.getAllHotels();
+    hotels.sort((a, b) => b.rooms - a.rooms);
+    console.log(hotels);
     res.render('home/home', { hotels })
 })
 
